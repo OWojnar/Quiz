c2fcba4208c9bad59d2f2f41d97b51897ef862aa
OWojnar <93475937+OWojnar@users.noreply.github.com>
2021-11-28 12:16:15 +0100

    Add files via upload

---
+++ Odpowiedz.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Odpowiedz
+    {
+        public int Id { get; set; }
+        public string Tresc { get; set; }
+        public bool CzyPrawidlowa { get; set; }
+    }
+}
---
+++ Pytanie.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Pytanie
+    {
+        public int Id { get; set; }
+        public int Kategoria { get; set; }
+        public string Tresc { get; set; }
+        public List<Odpowiedz> Odpowiedzi { get; set; } = new List<Odpowiedz>();
+    }
+}
